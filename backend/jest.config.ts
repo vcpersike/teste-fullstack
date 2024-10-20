1ac453ee4f8512537bd9e03a7347f0f155de9a49
vcpersike <vcpersike@gmail.com>
2024-10-20 03:43:21 -0300

    Apagando pastas

--- backend/jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['js', 'ts'],
-  transform: {
-    '^.+\\.ts?$': 'ts-jest',
-  },
-  testMatch: ['**/tests/**/*.test.ts'],
-};
--- backend/src/utils/IdGenerator.ts
+++
@@ -1,4 +0,0 @@
-export function gerarId(): string {
-    const randomDigits = Math.floor(1000 + Math.random() * 9000);
-    return `IC${randomDigits}OM`;
-}
